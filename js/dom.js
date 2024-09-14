275ec08012e858ed0b7be1227a34276cf44d52a8
Habibur Rahman <habibur88454@gmail.com>
2024-09-14 20:16:30 +0600

    get element by id and class name

--- js/dom.js
+++ js/dom.js
@@ -5,10 +5,20 @@ const h1Collection = document.getElementsByTagName('h1');
 for (const h1 of h1Collection) {
     // console.log(h1)
     const h1Text  = h1.innerText;
-    console.log(h1Text)
+    // console.log(h1Text)
 }
 
 const liCollection = document.getElementsByTagName('li');
 for(const li of liCollection){
-    console.log(li.innerText)
+    // console.log(li.innerText)
+}
+
+
+const playersTitle = document.getElementById('players-title');
+playersTitle.innerText = 'Lords players' 
+
+
+const heros = document.getElementsByClassName('hero');
+for(const hero of heros){
+    console.log(hero.innerText)
 }
