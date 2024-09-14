013d0332a45f930c4797e615888e155161bcaab9
Habibur Rahman <habibur88454@gmail.com>
2024-09-14 19:48:28 +0600

    get data using tag name

--- js/dom.js
+++ js/dom.js
@@ -1,2 +1,14 @@
-console.log('external js file')
-console.log(document)
+// console.log('external js file')
+// console.log(document);
+
+const h1Collection = document.getElementsByTagName('h1');
+for (const h1 of h1Collection) {
+    // console.log(h1)
+    const h1Text  = h1.innerText;
+    console.log(h1Text)
+}
+
+const liCollection = document.getElementsByTagName('li');
+for(const li of liCollection){
+    console.log(li.innerText)
+}
